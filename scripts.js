3f008c5da13e3338171b222d40cd5836a109df4f
Ghaith-Alkhatib <132254769+Ghaith-Alkhatib@users.noreply.github.com>
2024-06-27 16:44:26 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,26 +3,24 @@ AOS.init({
     duration: 1200,
 });
 
-// Function to handle section display on larger screens
+// Function to handle section display
 function showSection(sectionId) {
-    if (window.innerWidth > 768) {
-        // Hide all sections except "About Me" and "Contact"
-        document.querySelectorAll('.section').forEach(section => {
-            if (section.id !== 'about-me' && section.id !== 'contact') {
-                section.classList.add('hidden');
-            }
-        });
-
-        // Show the selected section
-        const selectedSection = document.getElementById(sectionId);
-        if (selectedSection) {
-            selectedSection.classList.remove('hidden');
+    // Hide all sections except "About Me" and "Contact"
+    document.querySelectorAll('.section').forEach(section => {
+        if (section.id !== 'about-me' && section.id !== 'contact') {
+            section.classList.add('hidden');
         }
+    });
+    
+    // Show the selected section
+    const selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+        selectedSection.classList.remove('hidden');
     }
 }
 
-// Event listeners for menu links
-document.querySelectorAll('.mobile-menu a, .sidebar a').forEach(link => {
+// Event listeners for header buttons
+document.querySelectorAll('.mobile-buttons a').forEach(link => {
     link.addEventListener('click', event => {
         event.preventDefault();
         const sectionId = event.target.getAttribute('data-section');
@@ -30,16 +28,6 @@ document.querySelectorAll('.mobile-menu a, .sidebar a').forEach(link => {
     });
 });
 
-// Show "About Me" and "Contact" sections by default on larger screens
-if (window.innerWidth > 768) {
-    showSection('about-me');
-    showSection('contact');
-}
-
-// Toggle mobile menu visibility
-const menuToggle = document.querySelector('.menu-toggle');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-menuToggle.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-});
+// Show "About Me" and "Contact" sections by default
+showSection('about-me');
+showSection('contact');
